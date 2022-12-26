e6cb658a902a9a584d2baf1bb2bf9565ee0a0fb1
YUNG\young <yungxatik228@mail.ru>
2022-12-26 16:01:11 +0300

    money and Diamonds UI

--- Assets/Scripts/Fisher/Money.cs
+++ Assets/Scripts/Fisher/Money.cs
@@ -19,11 +19,20 @@ public class Money : MonoBehaviour
     #endregion
 
     [field: SerializeField] public float MoneyCount { get; private set; }
+    [field: SerializeField] public float DiamondsCount { get; private set; }
+    
     [SerializeField] private TextMeshProUGUI moneyText;
     [SerializeField] private TextMeshProUGUI fishBookMoneyText;
+    [SerializeField] private TextMeshProUGUI shopMoneyText;
+    
+    [SerializeField] private TextMeshProUGUI shopDiamondsText;
+    [SerializeField] private TextMeshProUGUI missionDiamondsText;
 
     private Tween _addTween;
     private Tween _removeTween;
+
+    private Tween _diamondsAddTween;
+    private Tween _diamondsRemoveTween;
 
     private void Start()
     { 
@@ -34,14 +43,92 @@ public class Money : MonoBehaviour
     {
         moneyText.text = $"{MoneyCount}";
         fishBookMoneyText.text = $"{MoneyCount}";
+        shopMoneyText.text = $"{MoneyCount}";
+        shopDiamondsText.text = $"{DiamondsCount}";
+        missionDiamondsText.text = $"{DiamondsCount}";
     }
 
+    public void AddDiamonds(int value)
+    {
+        DOTween.To(x => DiamondsCount = x, DiamondsCount, DiamondsCount + value, 1f)
+            .OnUpdate((() => shopDiamondsText.text = $"{Mathf.RoundToInt(DiamondsCount)}")).OnComplete(delegate
+            {
+                shopDiamondsText.text = $"{Mathf.RoundToInt(DiamondsCount)}";
+            });
+
+        if (_diamondsAddTween == null)
+        {
+            _diamondsAddTween = DOTween.To(x => DiamondsCount = x, DiamondsCount, DiamondsCount + value, 1f)
+                .OnUpdate((() => missionDiamondsText.text = $"{Mathf.RoundToInt(DiamondsCount)}")).OnComplete(delegate
+                {
+                    missionDiamondsText.text = $"{DiamondsCount}";
+                });
+        }
+        else
+        {
+            if (_diamondsAddTween.IsActive())
+            {
+                _diamondsAddTween.Complete();
+                _diamondsAddTween = DOTween.To(x => DiamondsCount = x, DiamondsCount, DiamondsCount + value, 1f)
+                    .OnUpdate((() => missionDiamondsText.text = $"{Mathf.RoundToInt(DiamondsCount)}")).OnComplete(delegate
+                    {
+                        missionDiamondsText.text = $"{DiamondsCount}";
+                    });
+            }
+            else
+            {
+                _diamondsAddTween = DOTween.To(x => DiamondsCount = x, DiamondsCount, DiamondsCount + value, 1f)
+                    .OnUpdate((() => missionDiamondsText.text = $"{Mathf.RoundToInt(DiamondsCount)}")).OnComplete(delegate
+                    {
+                        missionDiamondsText.text = $"{DiamondsCount}";
+                    });
+            }
+        }
+    }
+    
+    public void RemoveDiamonds(int value)
+    {
+        if (_diamondsRemoveTween == null)
+        {
+            _diamondsRemoveTween = DOTween.To(x => DiamondsCount = x, DiamondsCount, DiamondsCount - value, 1f).
+                OnUpdate((() => shopDiamondsText.text =  $"{Mathf.RoundToInt(DiamondsCount)}")).OnComplete(delegate
+                {
+                    missionDiamondsText.text = $"{DiamondsCount}";
+                    shopDiamondsText.text = $"{DiamondsCount}";
+                });
+        }
+        else
+        {
+            if (_diamondsRemoveTween.IsActive())
+            {
+                _diamondsRemoveTween.Complete();
+                _diamondsRemoveTween = DOTween.To(x => DiamondsCount = x, DiamondsCount, DiamondsCount - value, 1f).
+                    OnUpdate((() => shopDiamondsText.text =  $"{Mathf.RoundToInt(DiamondsCount)}")).OnComplete(delegate
+                    {
+                        missionDiamondsText.text = $"{DiamondsCount}";
+                        shopDiamondsText.text = $"{DiamondsCount}";
+                    });
+            }
+            else
+            {
+                _diamondsRemoveTween = DOTween.To(x => DiamondsCount = x, DiamondsCount, DiamondsCount - value, 1f).
+                    OnUpdate((() => shopDiamondsText.text =  $"{Mathf.RoundToInt(DiamondsCount)}")).OnComplete(delegate
+                    {
+                        missionDiamondsText.text = $"{DiamondsCount}";
+                        shopDiamondsText.text = $"{DiamondsCount}";
+                    });
+            }
+        }
+    }
+    
     public void AddMoney(int value)
     {
          DOTween.To(x => MoneyCount = x, MoneyCount, MoneyCount + value, 1f)
             .OnUpdate((() => fishBookMoneyText.text = $"{Mathf.RoundToInt(MoneyCount)}")).OnComplete(delegate
             {
                 EventManager.OnMoneyAddedInvoke();
+                fishBookMoneyText.text = $"{Mathf.RoundToInt(MoneyCount)}";
+                shopMoneyText.text = $"{MoneyCount}";
             });
         
         if (_addTween == null)
@@ -50,6 +137,7 @@ public class Money : MonoBehaviour
                 .OnUpdate((() => moneyText.text = $"{Mathf.RoundToInt(MoneyCount)}")).OnComplete(delegate
                 {
                     EventManager.OnMoneyAddedInvoke();
+                    shopMoneyText.text = $"{MoneyCount}";
                 });
         }
         else
@@ -61,6 +149,7 @@ public class Money : MonoBehaviour
                     .OnUpdate((() => moneyText.text = $"{Mathf.RoundToInt(MoneyCount)}")).OnComplete(delegate
                     {
                         EventManager.OnMoneyAddedInvoke();
+                        shopMoneyText.text = $"{MoneyCount}";
                     });
             }
             else
@@ -69,6 +158,7 @@ public class Money : MonoBehaviour
                     .OnUpdate((() => moneyText.text = $"{Mathf.RoundToInt(MoneyCount)}")).OnComplete(delegate
                     {
                         EventManager.OnMoneyAddedInvoke();
+                        shopMoneyText.text = $"{MoneyCount}";
                     });
             }
         }
@@ -83,6 +173,7 @@ public class Money : MonoBehaviour
                 {
                     EventManager.OnMoneyRemovedInvoke(); 
                     fishBookMoneyText.text = $"{MoneyCount}";
+                    shopMoneyText.text = $"{MoneyCount}";
                 });
         }
         else
@@ -95,6 +186,7 @@ public class Money : MonoBehaviour
                     {
                         EventManager.OnMoneyRemovedInvoke(); 
                         fishBookMoneyText.text = $"{MoneyCount}";
+                        shopMoneyText.text = $"{MoneyCount}";
                     });
             }
             else
@@ -104,15 +196,20 @@ public class Money : MonoBehaviour
                     {
                         EventManager.OnMoneyRemovedInvoke(); 
                         fishBookMoneyText.text = $"{MoneyCount}";
+                        shopMoneyText.text = $"{MoneyCount}";
                     });
             }
         }
-
     }
 
     [ContextMenu("Test")]
     private void TestAdd()
     {
         AddMoney(1000);
+    }
+    [ContextMenu("TestDiamonds")]
+    private void TestDiamondsAdd()
+    {
+        AddDiamonds(1000);
     }
 }
